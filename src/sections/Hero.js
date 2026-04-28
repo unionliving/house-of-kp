59f69112f422be80241f7187f96406bcdb3247b9
Sushil Jadhav <sushiljadhav407@gmail.com>
2026-04-28 23:11:18 +0530

    first commit

---
+++ src/sections/Hero.js
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (<section id="home" className="relative bg-[#f6f0e8] ">
+      <div className="relative max-h-[180px] w-full md:max-h-[700px]">
+        <img src="/assets/hero-banner.webp" alt="Modern co-living space" className="block h-full w-full object-cover"/>
+
+        
+      </div>
+    </section>);
+};
+export default Hero;
